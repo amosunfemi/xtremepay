03bc4a62c9196b0504159fedeed7deb342c1897f
Sunday Amosun <sunday.amosun@gmail.com>
2015-11-05 05:05:48 +0000

    Commit for Auth Controller

--- Sources/services/src/xtremepay.com/backoffice/controllers/auth_controller.go
+++ Sources/services/src/xtremepay.com/backoffice/controllers/auth_controller.go
@@ -4,19 +4,38 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/mholt/binding"
+	"github.com/unrolled/render"
+
 	services "xtremepay.com/backoffice/logic"
 	models "xtremepay.com/backoffice/models/util"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
+	/*r := render.New(render.Options{})
+	user := new(models.User)
+	errs := binding.Bind(req, user)
+	if errs.Handle(res) {
+		r.JSON(res, 422, errs.Error())
+		return
+	}
+	bindingErr := user.Validate(req, errs)
+	if bindingErr != nil {
+		r.JSON(res, 422, bindingErr.Error())
+		return
+	}*/
+	render := render.New(render.Options{})
 	requestUser := new(models.User)
-	decoder := json.NewDecoder(r.Body)
-	decoder.Decode(&requestUser)
-
+	errs := binding.Bind(r, requestUser)
+	if errs.Handle(w) {
+		render.JSON(w, 422, errs.Error())
+		return
+	}
 	responseStatus, token := services.Login(requestUser)
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(responseStatus)
-	w.Write(token)
+	render.JSON(w, responseStatus, token)
+	//w.Header().Set("Content-Type", "application/json")
+	//w.WriteHeader(responseStatus)
+	//w.Write(token)
 }
 
 func RefreshToken(w http.ResponseWriter, r *http.Request, next http.HandlerFunc) {
